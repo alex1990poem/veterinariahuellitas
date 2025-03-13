b34461180462d3f0b9689cfe2d4469d27aef9b95
Alejamarin08 <mayra_amarin@soy.sena.edu.co>
2025-03-13 00:00:19 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VeterinarioController;
 use App\Http\Controllers\ProveedorController;
+use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\MascotaController;
 
 Route::apiResource('veterinarios', VeterinarioController::class);
 Route::apiResource('proveedores', ProveedorController::class);
+Route::apiResource('clientes', ClienteController::class);
 Route::apiResource('mascotas', MascotaController::class);
